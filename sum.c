ba132a9c1c1848d1c60485d8331cff895b777dea
donaldblesse <donaldblessengalla@gmail.com>
2021-07-13 14:25:57 +0100

    Program to add two numbers in C

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    int x,y,sum;
+    printf("enter numbers to add \n");
+    scanf("%d%d",&x,&y);
+    sum = (x+y);
+    printf("the sum is %d",sum);
+    return 0;
+}
