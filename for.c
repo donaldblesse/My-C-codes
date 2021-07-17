29e6c929a76234918d2d1ab5038fe505f78473de
donaldblesse <donaldblessengalla@gmail.com>
2021-07-17 14:15:47 +0100

    Entering numbers in an array.

---
+++ for.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+
+    int i,num;
+    printf("Enter size of array \n");
+    scanf("%d",&num);
+    int arr[num];
+    for(i=0;i<num;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+}
